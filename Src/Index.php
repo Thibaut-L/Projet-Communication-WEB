038d1e59482952ba37a04500f7979baf59b55845
agathevld <agathe.vl@hotmail.com>
2022-04-18 13:43:41 +0200

    Creation du header

--- Src/Index.php
+++ Src/Index.php
@@ -1,1 +1,17 @@
-<?php echo coucou; ?> 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+</head>
+   
+<body>
+    <?php include "header.php"; ?>
+<br>
+    
+    <a target="_blank" href="img_forest.jpg">
+    <img src="IMG_2611.jpeg" alt="image">
+    </a>
+</body>
+</html>
---
+++ Src/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+</head>
+   
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+  <li style="float:none"><a href="#about">Les histoires dont vous etes la.e protagoniste.</a></li>
+  
+</ul>
+
+
+</body>
+</html>
+
