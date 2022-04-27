5a16576459f1531d43a570a3e3773376d20d114f
thibautlacroix <thilacroix@ensc.fr>
2022-04-27 17:17:52 +0200

    Create AddStory.php

---
+++ Src/AddStory.php
@@ -0,0 +1,36 @@
+<?php 
+require_once('Functions.php');
+session_start(); 
+
+if(isUserConnected()) {
+
+    if(isset($_POST['title'])){
+        $title = escape($_POST['title']);
+        $storyNumber = escape($_POST['StoryID']); 
+        $
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
