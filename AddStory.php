6a5f5d07c9f189a661ed81c02833931d899c361b
thibautlacroix <thilacroix@ensc.fr>
2022-05-04 11:34:46 +0200

    ConnectDB

--- AddStory.php
+++ AddStory.php
@@ -1,91 +1,105 @@
-<?php 
+<?php
 require_once('includes/functions.php');
-session_start(); 
-if(isUserConnected()) {
-  if (isset($_POST['title'])){
-    $title = escape($_POST['title']);
-    $storyID = escape($_POST['StoryID']); 
-    $chapterText = escape($_POST['chapterText']); 
-    $chapterId = escape($_POST['chapterId']);
-    $synopsis = escape($_POST['Synopsis']);
-    $auteur = escape($_POST['auteur']);
-    $chapter = escape($_POST['chapter']); 
-  }
+session_start();
+if (isUserConnected()) {
+    if (isset($_POST['title'])) {
+        $title = escape($_POST['title']);
+        $storyID = escape($_POST['StoryID']);
+        $chapterText = escape($_POST['chapterText']);
+        $chapterId = escape($_POST['chapterId']);
+        $synopsis = escape($_POST['Synopsis']);
+        $auteur = escape($_POST['auteur']);
+        $chapter = escape($_POST['chapter']);
+    }
 }
 ?>
-<!doctype html>
-<html> 
+    <!doctype html>
+    <html>
 
-<?php
+    <?php
     $pageTitle = "Ajout d'une histoire";
     require_once "includes/head.php";
     ?>
 
     <body>
     <?php require_once "includes/header.php"; ?>
-      <div class="container">
+    <div class="container">
 
 
-          <h2 class="text-center">Ajout d'une histoire</h2>
-          <div class="well">
+        <h2 class="text-center">Ajout d'une histoire</h2>
+        <div class="well">
             <form class="form-horizontal" role="form" enctype="multipart/form-data" action="AddStory.php" method="post">
-              <input type="hidden" name="id" value="<?= $storyID ?>">
-              <div class="form-group">
-                <label class="col-sm-4 control-label">Le titre de votre histoire</label>
-                <div class="col-sm-6">
-                  <input type="text" name="title" value="<?= $title ?>" class="form-control" placeholder="Entrez le titre de votre histoire" required autofocus>
+                <input type="hidden" name="id" value="<?= $storyID ?>">
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">Le titre de votre histoire</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="title" value="<?= $title ?>" class="form-control"
+                               placeholder="Entrez le titre de votre histoire" required autofocus>
+                    </div>
                 </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-4 control-label">Synopsis</label>
-                <div class="col-sm-6">
-                  <textarea name="shortDescription" class="form-control" placeholder="Entrez sa description courte" required>
-                    <?= $synopsis?>
+                <div class ="form-group">
+                    <label class="col-sm-4 control-label">L'auteur de l'histoire </label>
+                    <div class="col-sm-6">
+                        <input type="text" name="title" value="<?= $auteur  ?>" class="form-control"
+                               placeholder="" required autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">Synopsis</label>
+                    <div class="col-sm-6">
+                  <textarea name="shortDescription" class="form-control" placeholder="Entrez sa description courte"
+                            required>
+                    <?= $synopsis ?>
                   </textarea>
+                    </div>
                 </div>
-              </div>
-              </div>
-          <div class="well">
-              <form class="form-horizontal" role="form" enctype="multipart/form-data" action="AddStory.php" method="post">
-                  <input type="hidden" name="id" value="<?= $storyID ?>">
-                  <div class="form-group">
-                      <label class="col-sm-4 control-label">Le titre de votre histoire</label>
-                      <div class="col-sm-6">
-                          <input type="text" name="title" value="<?= $title ?>" class="form-control" placeholder="Entrez le titre de votre histoire" required autofocus>
-                      </div>
-                  </div>
-                  <div class="form-group">
-                      <label class="col-sm-4 control-label">Synopsis</label>
-                      <div class="col-sm-6">
-                  <textarea name="shortDescription" class="form-control" placeholder="Entrez sa description courte" required>
-                    <?= $synopsis?>
+        </div>
+        <div class="well">
+            <form class="form-horizontal" role="form" enctype="multipart/form-data" action="AddStory.php" method="post">
+                <input type="hidden" name="id" value="<?= $storyID ?>">
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">Le titre de votre histoire</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="title" value="<?= $title ?>" class="form-control"
+                               placeholder="Entrez le titre de votre histoire" required autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">Synopsis</label>
+                    <div class="col-sm-6">
+                  <textarea name="shortDescription" class="form-control" placeholder="Entrez sa description courte"
+                            required>
+                    <?= $synopsis ?>
                   </textarea>
-                      </div>
-                  </div>
-          </div>
-              <br>
-              <div class="form-group">
-                <div class="col-sm-4 col-sm-offset-4">
-                  <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span> Enregistrer votre histoire </button>
+                    </div>
                 </div>
-              </div>
-            </form>
-          </div>
+        </div>
+        <br>
+        <div class="form-group">
+            <div class="col-sm-4 col-sm-offset-4">
+                <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span>
+                    Enregistrer votre histoire
+                </button>
+            </div>
+            "
+        </div>
+        </form>
+    </div>
 
-          <?php //require_once "includes/footer.php"; ?>
-      </div>
+    <?php //require_once "includes/footer.php"; ?>
+    </div>
 
-      <?php require_once "includes/scripts.php"; ?>
+    <?php require_once "includes/scripts.php"; ?>
     </body>
-    
 
-  </html>
 
-  <?php// } else { ?>
+    </html>
+
+<? php// } else { ?>
     <html>
 
     <body>
     </body>
 
     </html>
-    <?php// } ?>
+<? php// } ?>
---
+++ includes/ConnectDB.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $BDD = new PDO("mysql:host=localhost;dbname=mymovies;charset=utf8","mymovies_user","secret",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e){
+    die('Erreur fatale : '.$e->getMessage());
+}
