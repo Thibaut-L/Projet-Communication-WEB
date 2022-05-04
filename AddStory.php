e81113512503109f65f4a7792b41723fd7bf99e1
thibautlacroix <thilacroix@ensc.fr>
2022-05-04 10:57:31 +0200

    Css changes

--- AddStory.php
+++ AddStory.php
@@ -17,13 +17,14 @@ if(isUserConnected()) {
 <html> 
 
 <?php
-    $pageTitle = "Ajout d'un film";
+    $pageTitle = "Ajout d'une histoire";
     require_once "includes/head.php";
     ?>
 
     <body>
+    <?php require_once "includes/header.php"; ?>
       <div class="container">
-       <?php require_once "includes/header.php"; ?>
+
 
           <h2 class="text-center">Ajout d'une histoire</h2>
           <div class="well">
@@ -44,25 +45,25 @@ if(isUserConnected()) {
                 </div>
               </div>
               </div>
-              <div class="well">
+          <div class="well">
+              <form class="form-horizontal" role="form" enctype="multipart/form-data" action="AddStory.php" method="post">
+                  <input type="hidden" name="id" value="<?= $storyID ?>">
                   <div class="form-group">
-                      <label class="col-sm-4 control-label">Auteur de l'histoire </label>
+                      <label class="col-sm-4 control-label">Le titre de votre histoire</label>
                       <div class="col-sm-6">
-                          <input type="text" name="director" value="<?= $auteur ?>" class="form-control" placeholder="Entrez son auteur" required>
+                          <input type="text" name="title" value="<?= $title ?>" class="form-control" placeholder="Entrez le titre de votre histoire" required autofocus>
                       </div>
                   </div>
                   <div class="form-group">
-                      <label class="col-sm-4 control-label">D</label>
+                      <label class="col-sm-4 control-label">Synopsis</label>
                       <div class="col-sm-6">
-                  <textarea name="Chapter" class="form-control" placeholder="Écrivez ici votre histoire" required>
-                    <?= $chapter?>
+                  <textarea name="shortDescription" class="form-control" placeholder="Entrez sa description courte" required>
+                    <?= $synopsis?>
                   </textarea>
                       </div>
                   </div>
-              </div>
-
-
-              
+          </div>
+              <br>
               <div class="form-group">
                 <div class="col-sm-4 col-sm-offset-4">
                   <button type="submit" class="btn btn-default btn-primary"><span class="glyphicon glyphicon-save"></span> Enregistrer votre histoire </button>
