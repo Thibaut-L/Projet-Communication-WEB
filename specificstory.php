916f333d357fe04c2042713c798769df764aa000
thibautlacroix <thilacroix@ensc.fr>
2022-05-13 13:19:45 +0200

    c'était pas la fin en fait
    déso.php

--- specificstory.php
+++ specificstory.php
@@ -1,44 +1,106 @@
 <?php require_once "includes/ConnectDB.php"; 
 require_once "includes/functions.php"; 
 session_start();
-$histoires = $BDD->query('select * from Histoire order by IdHistoire desc');
-//$chapitres = $BDD->query('select * from Chapitre order by IdChapitre desc');
-?> 
+?>
+<?php
+$getIdHistoire = $_GET['idHistoire'];
+$getChapId = $_GET['IdChapitre'];
+var_dump($_GET);
+$req = $BDD -> prepare('SELECT Titre FROM Histoire WHERE idHistoire=?');
+$req -> execute(array($getIdHistoire));
+$titreHistoire = $req -> fetchColumn();
 
+$getContenu = $BDD -> prepare('SELECT Contenu FROM Chapitre WHERE IdChapitre=?');
+$getContenu -> execute(array($getChapId));
+$contenu = ($getContenu -> fetchColumn());
+
+$getIdChoix = $BDD -> prepare('SELECT * FROM Choix WHERE IdChapitre =?');
+$getIdChoix -> execute(array($getChapId));
+$idChoix = ($getIdChoix -> fetchAll());
+echo(count($idChoix));
+echo("dd");
+
+
+$getIdChap = $BDD -> prepare('SELECT IdParaSuivant FROM Choix WHERE IdChapitre =?');
+$getIdChap -> execute(array($getChapId));
+$idChapSuivant = ($getIdChap-> fetchAll());
+
+$getTextChoix = $BDD -> prepare('SELECT TextChoix FROM Choix WHERE IdChapitre =?');
+$getTextChoix -> execute(array($getChapId));
+$TextChoix = ($getTextChoix -> fetchAll());
+
+$getVictoire = $BDD -> prepare('SELECT chapVictoire FROM Chapitre WHERE IdChapitre=?');
+$getVictoire -> execute(array($getChapId));
+$Victoire = ($getVictoire -> fetchColumn());
+
+$getDefaite = $BDD -> prepare('SELECT ChapDéfaite FROM Chapitre WHERE IdChapitre=?');
+$getDefaite -> execute(array($getChapId));
+$Defaite = ($getDefaite -> fetchColumn());
+
+
+
+
+
+?>
+<?php
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
+
 <?php require_once "includes/head.php"; ?>
+
+
+<title>Accueill</title>
+<body class="fondimage">
 <?php require_once "includes/header.php"; ?>
-<title>Accueil</title>
-<body>
-  <div id="head">
-			<div class="row">
-                <?php foreach ($histoires as $histoire){?>
-				<h1 class="lead"><?= $histoire['Titre'] ?></h1>
-				<p class="tagline"> est ce que ca marche </p>
-                <?php}?>
-			</div>
-	</div>
-  <br>
-  <div class="container text-center">
-		<br> <br>
-        <?php foreach ($chapitres as $chapitre){?>
-		<h2 class="thin"> Chapitre <?= $chapitre['IdChapitre'] ?></h2>
-		<p class="text-muted">
-        <?= $chapitre['Contenu'] ?>
-		</p>
-        <?php}?>
-	</div>
-  <br>
-  
-  
-        
+
+<div class="container">
+    <br>
+    <div class="well col-xl-xl" id="blackground">
+        <h2 class="text-center"><?php echo ($titreHistoire); ?> </h2>
+
+            <br>
+        <p><small> <?php echo $contenu ?>  </small></p>
+            <br>
+
+                <br>
+        <?php
+        foreach ($idChoix as $aIdChoix)
+        {
+            ?>
+            <a href="specificstory.php?idHistoire=<?php  echo $getIdHistoire; ?>&IdChapitre=<?php echo $aIdChoix['IdParaSuivant'];  ?>" > <?php echo $aIdChoix['TextChoix']; ?> </a>
+
+            <br/>
+            <?php
+        }
+        ?>
+        <?php if($Defaite == 1 ){ ?>
+            <h2><a href="story.php" >Tu as perdu pas de chance, clique ici pour revenir à l'accueil </a> </h2>
+
+        <?php } ?>
+        <?php if($Victoire == 1 ){ ?>
+            <h2><a href="story.php" >Tu as perdu pas de chance, clique ici pour revenir à l'accueil </a> </h2>
+
+        <?php } ?>
+
+
+    </div>
+</div>
 
 
 
-  
+
+<?php
+
+?>
+
+
   <?php require_once "includes/footer.php"; ?>
   <?php require_once "includes/scripts.php"?>  
 </body>
 </html>
-
